e07fccb8a82c040bba4e6d4327e86c01c7595be4
Tej Magar <tejmagar20510@gmail.com>
2024-07-06 10:29:29 +0545

    chore: refactor JsonResponse

--- lib/src/main/java/org/tinyweb/response/JsonResponse.java
+++ lib/src/main/java/org/tinyweb/response/JsonResponse.java
@@ -1,60 +1,15 @@
 package org.tinyweb.response;
 
-import androidx.annotation.NonNull;
-
-import org.tinyweb.headers.Headers;
 import org.tinyweb.response.status.Status;
-import org.tinyweb.response.status.StatusUtil;
 
-import java.nio.charset.StandardCharsets;
-
-public class JsonResponse extends Response {
-    private final Headers headers = new Headers();
-    private final String text;
-
-    private final StatusUtil.ResponseStatus responseStatus;
+public class JsonResponse extends HttpResponse {
 
     public JsonResponse(String text) {
-        this.responseStatus = StatusUtil.fromStatus(Status.Ok);
-        this.text = text;
-        initHeaders();
+        super(text);
     }
 
     public JsonResponse(Status status, String text) {
-        this.responseStatus = StatusUtil.fromStatus(status);
-        this.text = text;
-        initHeaders();
-    }
-
-    private void initHeaders() {
-        headers.set("Content-Length", String.valueOf(text.length()));
-        headers.set("Content-Type", "application/json");
-    }
-
-    @Override
-    public StatusUtil.ResponseStatus responseStatus() {
-        return responseStatus;
-    }
-
-    @NonNull
-    @Override
-    public Headers headers() {
-        return headers;
-    }
-
-    @Override
-    public boolean serveDefault() {
-        return true;
-    }
-
-    @Override
-    public boolean keepAlive() {
-        return true;
-    }
-
-    @NonNull
-    @Override
-    public byte[] body() {
-        return text.getBytes(StandardCharsets.UTF_8);
+        super(status, text);
+        headers().set("Content-Type", "application/json");
     }
 }
